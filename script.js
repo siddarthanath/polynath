ee7631c210acb2ed80e070ec170b446bfbc2770f
Siddartha Nath <sidnath01@hotmail.com>
2023-11-15 23:50:05 +0000

    Add header.

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log('PolyNath website loaded!');
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Other code...
+
+    // Typewriter effect
+    var text = "Education for Everyone";
+    var i = 0;
+    var dynamicText = document.getElementById('dynamic-text');
+
+    function typeWriter() {
+        if (i < text.length) {
+            dynamicText.innerHTML += text.charAt(i);
+            i++;
+            setTimeout(typeWriter, 150); // Adjust for typing speed
+        }
+    }
+
+    typeWriter(); // Start the effect
+});
