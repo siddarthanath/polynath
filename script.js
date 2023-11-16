b8e7233f27120697448110747ef5e1b081aad39f
Siddartha Nath <sidnath01@hotmail.com>
2023-11-16 00:02:41 +0000

    Add main body.

--- script.js
+++ script.js
@@ -20,3 +20,28 @@ document.addEventListener('DOMContentLoaded', function() {
 
     typeWriter(); // Start the effect
 });
+
+function openTab(tabName) {
+    var i, tabcontent, tabheaders;
+
+    // Get all elements with class="tab-content" and hide them
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tab-header" and remove the "active" class
+    tabheaders = document.getElementsByClassName("tab-header");
+    for (i = 0; i < tabheaders.length; i++) {
+        tabheaders[i].className = tabheaders[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName + "-content").style.display = "block";
+    document.getElementById(tabName + "-tab").className += " active";
+}
+
+// Set the "Learn" tab as active on page load
+document.addEventListener('DOMContentLoaded', function() {
+    openTab('learn'); // Default to showing the 'Learn' tab content
+});
