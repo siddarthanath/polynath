4550a6606c3e73ce836443eb5d7fe6b9c5b93b34
Siddartha Nath <sidnath01@hotmail.com>
2023-11-21 23:54:44 +0000

    Update ABOUT.

--- mathematics.js
+++ mathematics.js
@@ -1,15 +1,6 @@
 function showContent(tabId) {
-    // Placeholder function to demonstrate content switching
-    var content = document.getElementById('content');
-    if (tabId == "pre-university") {
-    content.innerHTML = <h2>Pre-University</h2>;
-    }
-    else if (tabId == "university") { content.innerHTML = 'Content for ' + tabId;
-
-    }
-    else { content.innerHTML = 'Content for ' + tabId;
-
-    };
+    // Placeholder function to demonstrate content switching{
+    content.innerHTML = document.getElementById(tabId);
 }
 
 window.onload = function() {
