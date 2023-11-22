1e90729e104a4abaaab8105959eb714b4bba5be9
Siddartha Nath <sidnath01@hotmail.com>
2023-11-22 10:53:21 +0000

    Update ABOUT.

--- mathematics.js
+++ mathematics.js
@@ -1,8 +1,20 @@
 function showContent(tabId) {
-    // Placeholder function to demonstrate content switching{
-    content.innerHTML = document.getElementById(tabId);
+    // Get all tabs and remove 'active' class
+    var tabs = document.getElementsByClassName('tab');
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].classList.remove('active');
+    }
+    
+    // Set the clicked tab as active
+    var activeTab = document.getElementById(tabId);
+    activeTab.classList.add('active');
+    
+    // Change the content
+    var content = document.getElementById('content');
+    content.innerHTML = 'Content for ' + tabId;
 }
 
+// Set default active tab and content on page load
 window.onload = function() {
     showContent('pre-university');
 };
