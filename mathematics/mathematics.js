99b844cc6e0abd8d69ed70780e7c04683fa2df98
Siddartha Nath <sidnath01@hotmail.com>
2023-11-25 18:45:56 +0000

    Update ABOUT.

--- mathematics/mathematics.js
+++ mathematics/mathematics.js
@@ -24,13 +24,6 @@ function showContent(tabId) {
                     <button class="curved-box expand-btn">
                         Edexcel
                     </button>
-                    <div class="uni-content">
-                        <!-- Add your hyperlinks here -->
-                        <div class="curved-box" onclick="">Past Papers</div>
-                        <div class="curved-box" onclick="alert('Under construction!')">Worksheets</div>
-                        <div class="curved-box" onclick="alert('Under construction!')">Notes & Videos</div>
-                        <!-- Add more links as needed -->
-                    </div>
                     <div class="curved-box" onclick="alert('Under construction!')">OCR MEI</div>
                     </div>
                 </div>
