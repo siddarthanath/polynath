a178cb57a68049b419b47b008debe9f0832b2e8d
Siddartha Nath <sidnath01@hotmail.com>
2023-11-26 01:13:22 +0000

    Update ABOUT.

--- mathematics/mathematics.js
+++ mathematics/mathematics.js
@@ -132,19 +132,24 @@ function showContent(tabId) {
 }
 
 function toggleDropdown(clickedElement) {
+    var dropdown = clickedElement.nextElementSibling;
+    var shouldScroll = dropdown.style.display === 'none' || dropdown.style.display === '';
+
     // First, we hide all dropdowns except the one that is clicked
     document.querySelectorAll('.pre-uni-content').forEach(function(content) {
-        if (content !== clickedElement.nextElementSibling) {
+        if (content !== dropdown) {
             content.style.display = 'none';
         }
     });
 
     // Then, we toggle the display of the clicked dropdown
-    var dropdown = clickedElement.nextElementSibling;
-    if (dropdown.style.display === 'none' || dropdown.style.display === '') {
-        dropdown.style.display = 'block';
-    } else {
-        dropdown.style.display = 'none';
+    dropdown.style.display = shouldScroll ? 'block' : 'none';
+
+    // If the dropdown is being shown, scroll it into view
+    if (shouldScroll) {
+        setTimeout(function() { // Set a timeout to allow for dropdown to render
+            dropdown.scrollIntoView({ behavior: 'smooth', block: 'end' });
+        }, 0);
     }
 }
 
