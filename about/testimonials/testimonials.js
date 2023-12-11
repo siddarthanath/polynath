16a3a53752a32053525aacd66ee790210160851c
Siddartha Nath <sidnath01@hotmail.com>
2023-12-11 22:29:15 +0000

    Update ABOUT.

--- about/testimonials/testimonials.js
+++ about/testimonials/testimonials.js
@@ -1,35 +1,36 @@
-const testimonials = [
-    {
-      text: "Rowan from Vantage is the real deal: a rare blend of being a sufficiently brilliant mathematician to have a complete, deep and intuitive grasp on the difficult problems set in admissions tests, but also a great teacher who is able to explain the thought process in a way students can understand.",
-      author: "Parent of a STEP student who achieved 1 in STEP 2 and S in STEP 3"
-    },
-    {
+document.addEventListener('DOMContentLoaded', function() {
+    const testimonials = [
+      {
         text: "Rowan from Vantage is the real deal: a rare blend of being a sufficiently brilliant mathematician to have a complete, deep and intuitive grasp on the difficult problems set in admissions tests, but also a great teacher who is able to explain the thought process in a way students can understand.",
         author: "Parent of a STEP student who achieved 1 in STEP 2 and S in STEP 3"
-    },
-  ];
+      },
+      {
+        text: "Sid from Vantage is the real deal: a rare blend of being a sufficiently brilliant mathematician to have a complete, deep and intuitive grasp on the difficult problems set in admissions tests, but also a great teacher who is able to explain the thought process in a way students can understand.",
+        author: "Parent of a STEP student who achieved 1 in STEP 2 and S in STEP 3"
+      },
+      // ... Add more testimonials as needed
+    ];
   
-  let currentTestimonialIndex = 0;
+    let currentTestimonialIndex = 0; // This will start with the first testimonial
   
-  function updateTestimonial(index) {
-    const testimonialText = document.querySelector('.testimonial-text');
-    const testimonialAuthor = document.querySelector('.testimonial-author');
-    testimonialText.innerText = testimonials[index].text;
-    testimonialAuthor.innerText = testimonials[index].author;
-  }
+    function updateTestimonial(index) {
+      const testimonialText = document.querySelector('.testimonial-text');
+      const testimonialAuthor = document.querySelector('.testimonial-author');
+      testimonialText.innerText = testimonials[index].text;
+      testimonialAuthor.innerText = testimonials[index].author;
+    }
   
-  document.getElementById('prev').addEventListener('click', function() {
-    currentTestimonialIndex = (currentTestimonialIndex - 1 + testimonials.length) % testimonials.length;
+    // Initialize with the first testimonial
     updateTestimonial(currentTestimonialIndex);
-  });
   
-  document.getElementById('next').addEventListener('click', function() {
-    currentTestimonialIndex = (currentTestimonialIndex + 1) % testimonials.length;
-    updateTestimonial(currentTestimonialIndex);
-  });
+    document.getElementById('prev').addEventListener('click', function() {
+      currentTestimonialIndex = (currentTestimonialIndex - 1 + testimonials.length) % testimonials.length;
+      updateTestimonial(currentTestimonialIndex);
+    });
   
-  // Initialize with the first testimonial
-  document.addEventListener('DOMContentLoaded', () => {
-    updateTestimonial(currentTestimonialIndex);
+    document.getElementById('next').addEventListener('click', function() {
+      currentTestimonialIndex = (currentTestimonialIndex + 1) % testimonials.length;
+      updateTestimonial(currentTestimonialIndex);
+    });
   });
   
