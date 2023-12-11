e6572bd79401f695cadc49e575523425d83dcec6
Siddartha Nath <sidnath01@hotmail.com>
2023-12-11 20:44:04 +0000

    Update ABOUT.

---
+++ about/testimonials/testimonials.js
@@ -0,0 +1,8 @@
+document.getElementById('prev').addEventListener('click', function() {
+    // Code to navigate to the previous testimonial
+  });
+  
+  document.getElementById('next').addEventListener('click', function() {
+    // Code to navigate to the next testimonial
+  });
+  
