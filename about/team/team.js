ffb14cd6c1e82431cb036718ed6fe682156df1f5
Siddartha Nath <sidnath01@hotmail.com>
2023-12-20 11:19:14 +0000

    Update ABOUT.

---
+++ about/team/team.js
@@ -0,0 +1,12 @@
+  document.addEventListener('DOMContentLoaded', function() {
+    const fadeInElements = document.querySelectorAll('.fade-in');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    });
+  });
+  
