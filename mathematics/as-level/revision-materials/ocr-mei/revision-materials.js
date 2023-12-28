efb5fc7c5ae1c18fcbef36337eb41b6ba8edca9d
Siddartha Nath <sidnath01@hotmail.com>
2023-12-28 00:27:21 +0000

    Update ABOUT.

---
+++ mathematics/as-level/revision-materials/ocr-mei/revision-materials.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Call showContent to initialize the default content
+    const fadeInElements = document.querySelectorAll('.fade-in');
+  
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    });
+  
+    fadeInElements.forEach(element => {
+        observer.observe(element);
+    });
+  })
+
+function toggleDropdown(clickedElement) {
+    // Call showContent to initialize the default content
+    // First, we hide all dropdowns except the one that is clicked
+    document.querySelectorAll('.pre-uni-content').forEach(function(content) {
+        if (content !== clickedElement.nextElementSibling) {
+            content.style.display = 'none';
+        }
+    });
+
+    // Then, we toggle the display of the clicked dropdown
+    var dropdown = clickedElement.nextElementSibling;
+    if (dropdown.style.display === 'none' || dropdown.style.display === '') {
+        dropdown.style.display = 'block';
+    } else {
+        dropdown.style.display = 'none';
+    }
+}
+
+function openWebPage(url) {
+    window.open(url, '_blank');
+}
